ca274c5f0e0880497063c63223125ba4cb388d1e
cacaoMath <mahiro.1192@gmail.com>
2022-01-26 19:50:46 +0900

    update

--- src/init-firebase.js
+++ src/init-firebase.js
@@ -5,6 +5,7 @@ const firebaseConfig = {
     storageBucket: "haselab-flow-chat.appspot.com",
     messagingSenderId: "272062347835",
     appId: "1:272062347835:web:bcb7dcff8ead6b82ed3a1e",
+    databaseURL:"https://haselab-flow-chat-default-rtdb.asia-southeast1.firebasedatabase.app/",
     measurementId: "G-94RR9DC9H7"
   };
 
