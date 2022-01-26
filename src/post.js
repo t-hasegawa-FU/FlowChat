4dc8268cb986d2f0f1010bdfdb5e1ace3bc1d1e7
cacaoMath <mahiro.1192@gmail.com>
2022-01-26 19:14:06 +0900

    update

--- src/post.js
+++ src/post.js
@@ -1,14 +1,3 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAYogMcVUqjPkbrltK02NGe_cGQlmO04AY",
-  authDomain: "haselab-flow-chat.firebaseapp.com",
-  projectId: "haselab-flow-chat",
-  storageBucket: "haselab-flow-chat.appspot.com",
-  messagingSenderId: "272062347835",
-  appId: "1:272062347835:web:bcb7dcff8ead6b82ed3a1e",
-  measurementId: "G-94RR9DC9H7"
-};
-const app = initializeApp(firebaseConfig);
-  
   // Get a reference to the database service
 const database = app.database();
 
