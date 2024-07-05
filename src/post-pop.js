9def59711aeb984a99c3974b69386b5d1ed43770
t-hasegawa-FU <t-hase@g.u-fukui.ac.jp>
2024-07-05 16:14:19 +0900

    test

--- src/post-pop.js
+++ src/post-pop.js
@@ -6,34 +6,8 @@ const today = ""+td.getFullYear() +"_"+ (td.getMonth()+1)+"_" + td.getDate() + "
 let logInUser = "none";
 const colors = ["#d2691e", "#ff0000", "#0000ff", "#000000"]
 
-
-/*
 //以下でログイン処理を行う，ポップアップログインに変更（Firebaseの仕様変更）．
 window.onload = function(){
-  firebase.auth().getRedirectResult().then(function(result) {
-    logInUser = result.user;
-    }).catch(function(error) {
-    console.log('failure in Redirect');
-    });
-    
-    firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      logInUser = user.uid;
-      document.getElementById("User").innerText = user.email +"でログインしています";
-    } else {
-
-      console.log('no loginn user');
-      let provider = new firebase.auth.GoogleAuthProvider();
-      provider.setCustomParameters({
-        hd: 'g.u-fukui.ac.jp'
-      });
-      firebase.auth().signInWithPopup(provider);
-    }
-    });
-}
-*/
-  //ボタンでGoogle Auth
-  document.getElementById("logginButton").onclick =function (e){
       alert("test");
       let provider = new firebase.auth.GoogleAuthProvider();
       provider.setCustomParameters({
@@ -43,14 +17,14 @@ window.onload = function(){
           .then((result) => {
               alert(result);
               const user = result.user;
-              alert(user);
+              alert(firebase.auth.user);
               document.getElementById("User").innerText = user + "でログインしています．";
           }).catch((error) => {
               const errorCode = error.code;
               const errorMessage = error.message;
               document.getElementById("User").innerText = errorCode + " : " + errorMessage;
           });
-  }
+}
 
   //realtime databaseに書き込む
 function writeUserData(comment) {
